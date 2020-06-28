9df59efa4b925d7b057c07e3694afdab2025a87b
Dan <dcox38@gmailL.com>
2020-06-28 10:37:27 -0500

    created 3 routes

--- app/controllers/api/example_pages_controller.rb
+++ app/controllers/api/example_pages_controller.rb
@@ -1,2 +1,14 @@
 class Api::ExamplePagesController < ApplicationController
+    def hello   
+        render json: {message: "Well hello there, what a lovely Sunday this is."}
+    end
+
+    def hi_there
+        render json: {message: "This is super cool."}
+    end
+
+    def aloha
+        render json: {message: "Coding is so crazy and I have no idea what is happening."}
+    end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,14 @@ Rails.application.routes.draw do
   # namespace :api do
   #   get "/photos" => "photos#index"
   # end
+
+  namespace :api do
+    get "/hi" => "example_pages#hello"
+
+    get "/yo" => "example_pages#hi_there"
+
+    get "/fu" => "example_pages#aloha"
+
+  end
+
 end
